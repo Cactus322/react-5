12af8959a93ca094716ecb3fd37a6d80f93c0bcd
Gor <58237309+Cactus322@users.noreply.github.com>
2022-09-29 01:50:23 +0300

    little code refactored

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { useState, useEffect } from "react";
-import Blog from "./components/Blog";
-import { BlogForm } from "./components/BlogForm";
-import { Messages } from "./components/Messages";
-import { UserInfo } from "./components/UserInfo";
+import { BlogUserBlock } from "./components/BlogUserBlock";
+import { LoginForm } from "./components/LoginForm";
 import blogService from "./services/blogs";
-import loginService from "./services/login";
 
 const App = () => {
     const [blogs, setBlogs] = useState([]);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [user, setUser] = useState(null);
-    const [title, setTitle] = useState(null);
-    const [author, setAuthor] = useState(null);
-    const [url, setUrl] = useState(null);
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [url, setUrl] = useState('');
     const [errorMessage, setErrorMessage] = useState(null);
     const [successMessage, setSuccessMessage] = useState(null);
 
@@ -30,83 +27,34 @@ const App = () => {
         }
     }, []);
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-
-        try {
-            const user = await loginService.login({
-                username,
-                password,
-            });
-            window.localStorage.setItem(
-                "loggedBlogAppUser",
-                JSON.stringify(user)
-            );
-            blogService.setToken(user.token);
-            setUser(user);
-            setUsername("");
-            setPassword("");
-        } catch (exception) {
-            setErrorMessage("Wrong credentials");
-            setTimeout(() => {
-                setErrorMessage(null);
-            }, 3000);
-        }
-    };
-
-    const loginForm = () => (
-        <form onSubmit={handleLogin}>
-            <div>
-                <p>username</p>
-                <input
-                    type="text"
-                    value={username}
-                    name="Username"
-                    onChange={({ target }) => setUsername(target.value)}
-                />
-            </div>
-            <div>
-                <p>password</p>
-                <input
-                    type="password"
-                    value={password}
-                    name="Password"
-                    onChange={({ target }) => setPassword(target.value)}
-                />
-            </div>
-            <button type="submit">login</button>
-        </form>
-    );
-
-    const userBlock = () => (
-        <div>
-            <Messages errorMessage={errorMessage} successMessage={successMessage} />
-
-            <UserInfo user={user} setUser={setUser} />
-
-            <BlogForm
-                title={title}
-                author={author}
-                url={url}
-                setTitle={setTitle}
-                setAuthor={setAuthor}
-                setUrl={setUrl}
-                setErrorMessage={setErrorMessage}
-                setSuccessMessage={setSuccessMessage}
-            />
-
-            <h2>blogs</h2>
-
-            {blogs.map((blog) => (
-                <Blog key={blog.id} blog={blog} />
-            ))}
-        </div>
-    );
-
     return (
         <div>
-            {user === null && loginForm()}
-            {user !== null && userBlock()}
+            {user === null ? (
+                <LoginForm
+                    username={username}
+                    password={password}
+                    setUser={setUser}
+                    setUsername={setUsername}
+                    setPassword={setPassword}
+                    setErrorMessage={setErrorMessage}
+                />
+            ) : (
+                <BlogUserBlock
+                    blogs={blogs}
+                    user={user}
+                    title={title}
+                    author={author}
+                    url={url}
+                    errorMessage={errorMessage}
+                    successMessage={successMessage}
+                    setUser={setUser}
+                    setTitle={setTitle}
+                    setAuthor={setAuthor}
+                    setUrl={setUrl}
+                    setErrorMessage={setErrorMessage}
+                    setSuccessMessage={setSuccessMessage}
+                />
+            )}
         </div>
     );
 };
